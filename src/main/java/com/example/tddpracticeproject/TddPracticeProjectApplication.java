c8e9a195c9b2536de035433e00f5667bb0ae50ee
Uk-jake <mag0225@naver.com>
2024-12-13 10:43:19 +0900

    init

---
+++ src/main/java/com/example/tddpracticeproject/TddPracticeProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.tddpracticeproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TddPracticeProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TddPracticeProjectApplication.class, args);
+    }
+
+}
